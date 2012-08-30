a807f757f8b2cd513a14da0478e459c6ce9cb4f7
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-08-30 15:45:19 -0400

    version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     name='nflgame',
     author='Andrew Gallant',
     author_email='andrew@burntsushi.net',
-    version='1.0.3',
+    version='1.0.4',
     license='WTFPL',
     description='An API to retrieve and read NFL Game Center JSON data. '
                 'It can work with real-time data, which can be used for '
