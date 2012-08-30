195f109ecefe9d43cda5d4dffd94f0fcd6dcc1af
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-08-30 01:36:26 -0400

    These are useless. Examples are short and sweet and in the docs.

--- examples/add-players.py
+++
@@ -1,8 +0,0 @@
-import nflgame
-
-games = nflgame.games(2011)
-players = nflgame.combine(games)
-
-for p in players.passing().sort("passing_yds").limit(35):
-    print p, p.passing_yds, p.passing_cmp, p.passing_att, p.passing_tds
-
