9b2b8208ef2c87b8a5061244dbe1e7da3cd79cbd
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-08-30 02:19:10 -0400

    Grrr. Stupid fakeroot.

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ The package comes pre-loaded with game data from every pre- and regular season g
 
 However, if you try to search for data in a game that is being currently played, the JSON data will be downloaded from NFL.com at each request (so be careful not to inspect for data too many times while a game is being played). If you ask for data for a particular game that hasn't been cached to disk but is no longer being played, it will be automatically cached to disk so that no further downloads are required.'''
 
-docfiles = map(lambda s: 'doc/%s' % s, list(os.walk('doc'))[0][2])
+try:
+    docfiles = map(lambda s: 'doc/%s' % s, list(os.walk('doc'))[0][2])
+except IndexError:
+    docfiles = []
 
 setup(
     name='nflgame',
@@ -29,6 +32,7 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Other Audience',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Topic :: Database',
